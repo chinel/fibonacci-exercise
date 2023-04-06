2e059d78fb3578da40819b8e22479338d1e080af
nelo <nelokwaz@gmail.com>
2023-04-06 15:22:00 +0100

    add git ignore

--- src/test/hello.test.ts
+++
@@ -1,7 +0,0 @@
-import {hello} from "../main/hello";
-
-describe('When calling the hello function', function () {
-    it('should say "hello"', () => {
-        expect(hello()).toEqual('hello world');
-    })
-});
